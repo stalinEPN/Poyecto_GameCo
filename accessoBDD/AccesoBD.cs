fff18ca9722b5f13637efd30562cc2aceab24732
stalinEPN <156889530+stalinEPN@users.noreply.github.com>
2024-01-17 10:16:30 -0500

    Update AccesoBD.cs

--- accessoBDD/AccesoBD.cs
+++ accessoBDD/AccesoBD.cs
@@ -6,11 +6,11 @@ namespace accessoBDD {
         public static string conexion() {
             //cadena de conexion
 
-            string server = "ep-royal-dawn-84894339.us-east-2.aws.neon.fl0.io";
+            string server = "";
             string port = "5432";
-            string database = "GameCo";
-            string user = "fl0user";
-            string password = "X4zQgHSuAjm5";
+            string database = "";
+            string user = "";
+            string password = "";
 
             string cadenaCon = $@"Server = {server};
                                  Port = {port};
